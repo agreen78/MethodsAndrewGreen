f3be7b8c2187dfa1f9e615699def280f156e8171
agreen78 <andrewgreen78@gmail.com>
2020-02-28 09:44:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MethodsAndrewGreen
+{
+    class Program
+    {
+
+        static void MyName()
+
+        {
+            Console.WriteLine("My name is Andrew Green.");
+        }
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World!");
+            MyName();
+        }
+    }
+}
